2cc1500f71872266ba6c46a719c03b6e707ca216
degen-ape <112836832+degen-ape@users.noreply.github.com>
2022-12-07 02:29:08 +0000

    Add input options to the indicator. Use logger.

--- src/StrategyReport.js
+++ src/StrategyReport.js
@@ -1,16 +1,46 @@
 var TradingView = require("@mathieuc/tradingview")
 var fs = require('fs');
+const { createLogger, format, transports } = require('winston');
+const { splat, combine, timestamp, printf } = format;
 require('dotenv').config();
 
+// Setup logger
+const myFormat = printf( ({ level, message, timestamp , ...metadata}) => {
+  let msg = `${timestamp} [${level}] : ${message} `  
+  if(metadata) {
+	msg += JSON.stringify(metadata)
+  }
+  return msg
+});
+
+const logger = createLogger({
+  level: 'debug',
+  format: combine(
+	format.colorize(),
+	splat(),
+	timestamp(),
+	myFormat
+  ),
+  transports: [
+	  new transports.Console({ level: 'info' }),
+	  new transports.File({ filename: 'errors.log', level: 'error' }),
+  ]
+});
+module.exports = logger
+
 TradingView.getUser(process.env.TV_SESSION).then((user) => {
-    console.log(`TradingView user id: ${user.username}`);    
-    console.log(`TradingView user name: ${user.firstName} ${user.lastName}`);
+    logger.info(`TradingView user id: ${user.username}`);    
+    logger.info(`TradingView user name: ${user.firstName} ${user.lastName}`);
 });
 
 const pairs = ['BINANCE:BTCUSDT', 'BINANCE:DOTUSDT'];
+const timeframe = '1D';
+const range = 365*5;
 
 for (i in pairs)
 {
+    const pair = pairs[i]
+
     const client = new TradingView.Client({
         token: process.env.TV_SESSION,
         // DEBUG: true
@@ -24,10 +54,6 @@ for (i in pairs)
     // });
 
     const chart = new client.Session.Chart();
-    const pair = pairs[i];
-    const timeframe = '1D';
-    const range = 365*5;
-
     chart.setMarket(pair, {
         timeframe: timeframe,
         range: range,
@@ -35,46 +61,49 @@ for (i in pairs)
 
     TradingView.getIndicator('PUB;ryMeUolWwdyo9F3MNleMvPPmoSDpGY4n').then(async (indic) => {
 
-        console.log(`Loading '${indic.description}' study...`);
+        // Set indicator options
 
-        indic.setOption('commission_type', 'percent');
+        // Fees paid for each entry and exit
+        indic.setOption('commission_type', 'percent'); 
         indic.setOption('commission_value', 0.1);
-        indic.setOption('initial_capital', 100);
-        // indic.setOption('default_qty_value', 99);
-        // indic.setOption('default_qty_type', 'percent_of_equity');
-        // indic.setOption('currency', 'USD');
 
+        indic.setOption('_INDICATOR_SETTINGS_', true); 
+                
+        // The amount of funds initially available for the strategy to trade
+        indic.setOption('initial_capital', 100); 
+        
+        // Enables an additional calculation on bar close, allowing market orders to enter on the same tick the order is placed
+        indic.setOption('process_orders_on_close', true);         
+        
+        //The number of contracts/shares/lots/units per trade, or the amount in base currency, or a percentage of available equity
+        indic.setOption('default_qty_type', 'percent_of_equity');
+        indic.setOption('default_qty_value', 99);
+
+        // Calculate Short Trades under the Strategy Tester?
+        indic.setOption('Calculate_Short_Trades_under_the_Strategy_Tester', true);
+
+        // Activate SAFE_MODE (check additional criteria for Signals)
+        indic.setOption('_Activate_SAFE_MODE_check_additional_criteria_for_Signals', false);
+        // Type '1' for regular Safe Mode or Type '2' high safety mode
+        indic.setOption('Type_1_for_regular_Safe_Mode_or_Type_2_high_safety_mode', 1);
+        
+        // indic.setOption('currency', 'USDT'); // Can't set this only USD which is not what I want for my pairs.
+
+        // Print all input options to the indicator script, this is useful when I need to know what to set above.
         // console.log('inputs', JSON.stringify(indic.inputs, null, 4));
 
         const study = new chart.Study(indic);
         // console.log('study', JSON.stringify(study, null, 4));
 
-        // study.strategyReport.settings.dateRange.backtest
-        // console.log('Strategy report', JSON.stringify(study.strategyReport.settings.dateRange.backtest, null, 4));
-
-
         study.onUpdate(() => {
-            // console.log('Strategy report', JSON.stringify(study.strategyReport, null, 4));
 
             let exchange = pair.split(':')[0];
             let symbol = pair.split(':')[1];
 
             fs.writeFileSync(`output/${exchange}-${symbol}-${timeframe}.json`, JSON.stringify(study.strategyReport, null, 4));
-            // console.log('Prices periods:', JSON.stringify(chart.periods, null, 4));
-            // console.log('Study periods:', JSON.stringify(study.periods, null, 4));
-
-            // for (i in chart.periods) {
-            //   console.log(new Date(chart.periods[i].time*1000).toISOString())
-            // }
-            // console.log(new Date(study.strategyReport.settings.dateRange.backtest.from).toISOString());
-            // console.log(new Date(study.strategyReport.settings.dateRange.backtest.to).toISOString());
-
-            const performance = study.strategyReport.performance;
-            // console.log('performance', performance);
-
+         
             study.remove();
             client.end();
-            console.log('done');
         });
     });
 };
--- src/StrategyReportBatches.js
+++ src/StrategyReportBatches.js
@@ -2,7 +2,35 @@ var TradingView = require("@mathieuc/tradingview")
 var fs = require('fs');
 const { PromisePool } = require('@supercharge/promise-pool');
 const { createLogger, format, transports } = require('winston');
+const { splat, combine, timestamp, printf } = format;
 require('dotenv').config();
+
+const timeframe = '2D';
+const range = 365*3;
+
+   // Setup logger
+   const myFormat = printf( ({ level, message, timestamp , ...metadata}) => {
+    let msg = `${timestamp} [${level}] : ${message} `  
+    if(metadata) {
+      msg += JSON.stringify(metadata)
+    }
+    return msg
+  });
+  
+  const logger = createLogger({
+    level: 'debug',
+    format: combine(
+      format.colorize(),
+      splat(),
+      timestamp(),
+      myFormat
+    ),
+    transports: [
+        new transports.Console({ level: 'info' }),
+        new transports.File({ filename: 'errors.log', level: 'error' }),
+    ]
+  });
+  module.exports = logger
 
 function runBackTest(pair) {
     return new Promise((resolve, reject) => {
@@ -19,9 +47,7 @@ function runBackTest(pair) {
         // });
 
         const chart = new client.Session.Chart();
-        const timeframe = '2D';
-        const range = 365*3;
-
+       
         chart.setMarket(pair, {
             timeframe: timeframe,
             range: range,
@@ -29,9 +55,35 @@ function runBackTest(pair) {
 
         TradingView.getIndicator('PUB;ryMeUolWwdyo9F3MNleMvPPmoSDpGY4n').then(async (indic) => {
 
-            indic.setOption('commission_type', 'percent');
+            // Fees paid for each entry and exit
+            indic.setOption('commission_type', 'percent'); 
             indic.setOption('commission_value', 0.1);
-            indic.setOption('initial_capital', 100);
+
+            indic.setOption('_INDICATOR_SETTINGS_', true); 
+                    
+            // The amount of funds initially available for the strategy to trade
+            indic.setOption('initial_capital', 100); 
+            
+            // Enables an additional calculation on bar close, allowing market orders to enter on the same tick the order is placed
+            indic.setOption('process_orders_on_close', true);         
+            
+            //The number of contracts/shares/lots/units per trade, or the amount in base currency, or a percentage of available equity
+            indic.setOption('default_qty_type', 'percent_of_equity');
+            indic.setOption('default_qty_value', 99);
+
+            // Calculate Short Trades under the Strategy Tester?
+            indic.setOption('Calculate_Short_Trades_under_the_Strategy_Tester', true);
+
+            // Activate SAFE_MODE (check additional criteria for Signals)
+            indic.setOption('_Activate_SAFE_MODE_check_additional_criteria_for_Signals', false);
+            // Type '1' for regular Safe Mode or Type '2' high safety mode
+            indic.setOption('Type_1_for_regular_Safe_Mode_or_Type_2_high_safety_mode', 1);
+            
+            // indic.setOption('currency', 'USDT'); // Can't set this only USD which is not what I want for my pairs.
+
+            // Print all input options to the indicator script, this is useful when I need to know what to set above.
+            // console.log('inputs', JSON.stringify(indic.inputs, null, 4));
+
             const study = new chart.Study(indic);
 
             study.onError((...error) => {
@@ -48,28 +100,14 @@ function runBackTest(pair) {
 
                 study.remove();
                 client.end();
-                resolve("done")
             });
         });
     });
 };
 
 async function run () {
+    // Load token pairs from the symbols file.
     pairs = []
-
-    const logger = createLogger({
-        format: format.combine(format.timestamp(), format.json()),
-        transports: [new transports.Console(), new transports.File({ level: "error", filename: "errors.log" })],
-      });
-    
-    if (process.env.NODE_ENV !== "production") {
-        logger.add(
-          new transports.Console({
-            format: format.combine(format.colorize(), format.simple()),
-          })
-        );
-      }
-
     markets = JSON.parse(fs.readFileSync('output/binance-markets.json'));
     for (const m in markets) {
         pairs.push("BINANCE:".concat(markets[m].id));
